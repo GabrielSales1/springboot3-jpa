069b8e7b99797da9ea355bde1c6c0806ade518ca
Gabriel Sales <136273796+GabrielSales1@users.noreply.github.com>
2024-08-05 15:53:35 -0300

    Order, instant and date format (JSON FORMAT)

--- src/main/java/com/springweb/course/entities/User.java
+++ src/main/java/com/springweb/course/entities/User.java
@@ -1,12 +1,17 @@
 package com.springweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +29,10 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -35,6 +44,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	public Long getId() {
 		return id;
 	}
